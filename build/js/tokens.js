c31fc4ff5eb25c61b4b2e9d38510f5ae6889564d
henriqueAlgauer <algauer.henrique@gmail.com>
2024-08-03 17:02:39 -0300

    first

---
+++ build/js/tokens.js
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+module.exports = {
+  "ColorGray900": "#121214",
+  "VariableCollectionWhite": "#ffffff",
+  "VariableCollectionPurpel500": "#8215e6",
+  "VariableCollectionBlack": "#000000",
+  "VariableCollectionGray200": "#c1c1c4",
+  "VariableCollectionGray500": "#50505c",
+  "VariableCollectionGray800": "#1c1c1f",
+  "VariableCollectionGray900": "#121214"
+};
